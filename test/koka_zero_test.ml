0aff0d250ece48acc57acade24450ac518d7f875
Dan Gooding <dan.gooding2000@gmail.com>
2021-11-01 23:41:13 +0000

    Add a few tests of parser acceptance/errors

--- test/koka_zero_test.ml
+++ test/koka_zero_test.ml
@@ -1,16 +1,140 @@
+open Core
 open Alcotest
 
-let test_hello_with_name name () =
-  let greeting = Koka_zero.greet name in
-  let expected = "Hello " ^ name ^ "!" in
-  check string "same string" greeting expected
-;;
-
-let suite =
-  [ "can greet Tom", `Quick, test_hello_with_name "Tom"
-  ; "can greet John", `Quick, test_hello_with_name "John"
-  ; "can greet Dan", `Quick, test_hello_with_name "Dan"
+let correct_cases =
+  [ "single expression function", {|
+fun main() {
+  0;
+}
+|}
+  ; "toplevel value declaration", {|
+val number : int = 1729;
+|}
+  ; ( "dashes in identifiers"
+    , {|
+val kebab-case = 0;
+val x-y-z = 1;
+val number3-letter = 2;
+      |} )
+  ; "hex literals", {|
+val abcd = 0x1234;
+|}
+  ; "prime at end of identifier", {|
+val f' = diff(f);
+val f'' = diff(f')
+  |}
+  ; "boolean literals", {|
+val t = True;
+val f = False;
+  |}
+  ; "negative integer literals", {|
+val minus-fourty = -40;
+  |}
+  ; ( "operators"
+    , {|
+fun hypotenuse(a : int, b : int) {
+  val c-squared = a * a + b * b;
+  val c = isqrt(c-squared);
+  c;
+}
+val all = 12 + 33 * 44 - 36 / 4 + 91 % 7 + 11
+va inside = 0 <= x && x < 7 || 100 < x && x <= 9000;
+|}
+    )
+  ; ( "mixing infix & prefix operators"
+    , {|
+fun xnor(a, b) {
+  a && b || !a && !b;
+}
+val sum = 3 + -1 * -7;
+|} )
+  ; ( "if statements"
+    , {|
+fun if-example() {
+  if x % 2 == 0 then
+    123
+  elif y % 2 == 0 then
+    400 + 50 + 6
+  elif False
+    -1
+  else
+    1 + 1;
+}
+    |}
+    )
+  ; "nested if statements", {|
+fun i() {
+  if a then
+    if b then
+      c
+  else d;
+}
+|}
+  ; "dot application", {|
+fun dot-application() {
+  x.best.fst.pow(3).print;
+;
+  |}
+  ; ( "trailing lambda application"
+    , {|
+fun trailing-lambda() {
+  f(x,y,z) { alpha } fn(b) {beta} {gamma};
+  a.g(1).h(2) { zzz };
+}
+  |}
+    )
+  ; ( "with syntax"
+    , {|
+fun one(aa, bb, cc, dd) {
+  with aa;
+  with bb();
+  with cc(3);
+  with x <- dd(5);
+  println(x : string);
+}
+  |}
+      (* TODO: more complex with syntax example *) )
   ]
 ;;
 
-let () = Alcotest.run "koka-zero" [ "Koka_zero", suite ]
+let wrong_cases =
+  [ "function without parameters", {|
+fun compute-the-answer {
+  42;
+}
+|}
+  ; "dashes in wrong place in identifier", {|
+val n-3 = n - 3;
+val n- = n
+|}
+    (* TODO: more incorrect examples *)
+  ]
+;;
+
+let test_is_valid_syntax code () =
+  match Koka_zero.parse_string code with
+  | Ok _ast -> ()
+  | Error _message -> fail "expected successful parse"
+;;
+
+let test_is_not_valid_syntax code () =
+  match Koka_zero.parse_string code with
+  | Ok _ast -> fail "expected syntax error"
+  | Error _message -> ()
+;;
+
+let correct_suite =
+  List.map correct_cases ~f:(fun (title, code) ->
+      title, `Quick, test_is_valid_syntax code)
+;;
+
+let wrong_suite =
+  List.map wrong_cases ~f:(fun (title, code) ->
+      title, `Quick, test_is_not_valid_syntax code)
+;;
+
+let () =
+  Alcotest.run
+    "koka-zero"
+    [ "Valid syntax", correct_suite; "Incorrect syntax", wrong_suite ]
+;;
