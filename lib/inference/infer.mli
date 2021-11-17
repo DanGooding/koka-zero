bb823e07371a17baf32c204a1e20799f0014b457
Dan Gooding <dan.gooding2000@gmail.com>
2021-11-17 22:03:42 +0000

    Adjuist inference interface

--- lib/inference/infer.mli
+++ lib/inference/infer.mli
@@ -1,6 +1,6 @@
 (* TODO: is the aim to annotate the tree, or just to check it and then forget
    about types *)
+open Core
+open Koka_zero_util
 
-val infer_type : Minimal_syntax.Expr.t -> Type.Poly.t
-
-(* TODO: substitution monad? *)
+val infer_type : Minimal_syntax.Expr.t -> (Type.Poly.t, Static_error.t) Result.t
