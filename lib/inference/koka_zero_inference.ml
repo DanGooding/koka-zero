7cfbc908b3f4dd3b278be5ef0ceb0dc6d5355d95
Dan Gooding <dan.gooding2000@gmail.com>
2021-11-17 22:03:42 +0000

    Adjuist inference interface

---
+++ lib/inference/koka_zero_inference.ml
@@ -0,0 +1,4 @@
+module Type = Type
+module Minimal_syntax = Minimal_syntax
+
+let infer_type = Infer.infer_type
---
+++ lib/inference/koka_zero_inference.mli
@@ -0,0 +1,7 @@
+open Core
+open Koka_zero_util
+module Type = Type
+module Minimal_syntax = Minimal_syntax
+
+(* TODO: this should interface with [Koka_zero_parsing] *)
+val infer_type : Minimal_syntax.Expr.t -> (Type.Poly.t, Static_error.t) Result.t
