dee326e32a849bba7bb3e7470c31b4efc4388457
Dan Gooding <dan.gooding2000@gmail.com>
2021-12-10 12:17:28 +0000

    Implement substituion on effects

--- lib/inference/substitution.mli
+++ lib/inference/substitution.mli
@@ -15,25 +15,53 @@ val extend
   -> type_:Type.Mono.t
   -> [ `Ok of t | `Duplicate ]
 
+(** try to add a new effect metavariable and the effect it maps to, it should
+    not already exist *)
+val extend_effect
+  :  t
+  -> var:Effect.Metavariable.t
+  -> effect:Effect.t
+  -> [ `Ok of t | `Duplicate ]
+
 (** add a metavariable and its type, raising an exception if it already exists
     in the substitution*)
 val extend_exn : t -> var:Type.Metavariable.t -> type_:Type.Mono.t -> t
 
-(** add multiple new metavariable-type mappings, returning [`Duplicate] if any
-    of the metavariables already exist *)
+(** add an effect metavariable and its effect, raising an exception if it
+    already exists in the substitution*)
+val extend_effect_exn : t -> var:Effect.Metavariable.t -> effect:Effect.t -> t
+
+(** add multiple new metavariable -> type mappings, returning [`Duplicate] if
+    any of the metavariables already exist *)
 val extend_many
   :  t
   -> Type.Mono.t Type.Metavariable.Map.t
   -> [ `Ok of t | `Duplicate ]
 
-(** add multiple new metavariable-type mappings, raising an exception if any of
-    the metavariables already exist *)
+(** add multiple new effect metavariable -> effect mappings, returning
+    [`Duplicate] if any of the metavariables already exist *)
+val extend_many_effect
+  :  t
+  -> Effect.t Effect.Metavariable.Map.t
+  -> [ `Ok of t | `Duplicate ]
+
+(** add multiple new metavariable -> type mappings, raising an exception if any
+    of the metavariables already exist *)
 val extend_many_exn : t -> Type.Mono.t Type.Metavariable.Map.t -> t
+
+(** add multiple new effect metavariable -> type mappings, raising an exception
+    if any of the metavariables already exist *)
+val extend_many_effect_exn : t -> Effect.t Effect.Metavariable.Map.t -> t
 
 (** Lookup a metavariable's type. Like [apply] this recursively replaces
     metavariables in the result until the only unknown metavariables remain. If
     the queried metavariable itself is unknown, then the result is None *)
 val lookup : t -> Type.Metavariable.t -> Type.Mono.t option
+
+(** Lookup an effect metavariable's type. Like [apply] this recursively replaces
+    metavariables in the result until the only unknown metavariables remain. If
+    the queried metavariable itself is unknown, then the result is None *)
+val lookup_effect : t -> Effect.Metavariable.t -> Effect.t option
 
 (** Replace all metavaraibes with their known types. Any unknown metavariables
     will still be present in the result *)
