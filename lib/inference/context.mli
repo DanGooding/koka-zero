2d6bee3a03668974d26d5f2cee97648b01fe4b9a
Dan Gooding <dan.gooding2000@gmail.com>
2021-12-12 11:09:39 +0000

    Refactor handling of bindings

--- lib/inference/context.mli
+++ lib/inference/context.mli
@@ -1,18 +1,32 @@
+module Or_cannot_shadow : sig
+  (** holds the result of an operation which may fail, due to shadowing an
+      unshadowable binding *)
+  type 'a t =
+    [ `Ok of 'a
+    | `Cannot_shadow
+    ]
+  [@@deriving sexp]
+end
+
 (** represents a typing context, mapping [Minimal_syntax.Variable.t]s to
-    [Type.t]s *)
+    [Type.t]s, and tracking whether those variables can be shadowed *)
 type t [@@deriving sexp]
 
 (** add a new variable to the context, or shadow an existing one. This returns
-    [None] if [var] is not shadowable *)
-val extend : t -> var:Minimal_syntax.Variable.t -> type_:Type.t -> t option
+    [`Cannot_shadow] if [var] is not shadowable *)
+val extend
+  :  t
+  -> var:Minimal_syntax.Variable.t
+  -> type_:Type.t
+  -> t Or_cannot_shadow.t
 
 (** add a new variable, which cannot be shadowed later, to the context. This
-    returns [None] if [var] is already present *)
+    returns [`Cannot_shadow] if [var] is already present *)
 val extend_unshadowable
   :  t
   -> var:Minimal_syntax.Variable.t
   -> type_:Type.t
-  -> t option
+  -> t Or_cannot_shadow.t
 
 (** lookup a variable's type in the context *)
 val find : t -> Minimal_syntax.Variable.t -> Type.t option
