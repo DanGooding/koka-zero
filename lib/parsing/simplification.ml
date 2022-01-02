f6594a567c33abe7004ba82b7ba0becc5343ff01
Dan Gooding <dan.gooding2000@gmail.com>
2022-01-02 11:49:57 +0000

    Rename restriction to simplification

--- lib/parsing/simplification.ml
+++ lib/parsing/simplification.ml
@@ -24,85 +24,80 @@ let restrict_to_singleton ~description = function
 
 (* TODO: make sure actual syntax errors aren't reported as unsupported syntax *)
 
-let type_to_minimal_syntax (_t : Syntax.type_) : Type.Mono.t Or_static_error.t =
+let simplify_type (_t : Syntax.type_) : Type.Mono.t Or_static_error.t =
   failwith "not implemented"
 ;;
 
-let var_id_to_minimal_syntax (_x : Syntax.Var_id.t) : Min.Variable.t =
+let simplify_var_id (_x : Syntax.Var_id.t) : Min.Variable.t =
   (* TODO: do I need to send this to a variant to allow safe generation of fresh
      dummy names *)
   failwith "not implemented"
 ;;
 
-let var_id_to_minimal_syntax_effect_label (_x : Syntax.Var_id.t)
-    : Effect.Label.t
-  =
+let simplify_var_id_to_effect_label (_x : Syntax.Var_id.t) : Effect.Label.t =
   failwith "not implemented"
 ;;
 
-let identifier_to_minimal_syntax (x : Syntax.Identifier.t) : Min.Variable.t =
+let simplify_identifier (x : Syntax.Identifier.t) : Min.Variable.t =
   match x with
-  | Syntax.Identifier.Var x -> var_id_to_minimal_syntax x
+  | Syntax.Identifier.Var x -> simplify_var_id x
 ;;
 
-let literal_to_minimal_syntax (lit : Syntax.literal) : Min.Literal.t =
+let simplify_literal (lit : Syntax.literal) : Min.Literal.t =
   match lit with
   | Syntax.Unit -> Min.Literal.Unit
   | Syntax.Int i -> Min.Literal.Int i
   | Syntax.Bool b -> Min.Literal.Bool b
 ;;
 
-let parameter_id_to_minimal_syntax
+let simplify_parameter_id
     : Syntax.parameter_id -> Min.Variable.t Or_static_error.t
   = function
-  | Syntax.Parameter_id x -> identifier_to_minimal_syntax x |> Result.Ok
+  | Syntax.Parameter_id x -> simplify_identifier x |> Result.Ok
   | Syntax.Parameter_wildcard -> failwith "not implemented"
 ;;
 
-let parameter_to_minimal_syntax
+let simplify_parameter
     : Syntax.parameter -> (Min.Variable.t * Type.Mono.t) Or_static_error.t
   =
  fun { Syntax.id; type_ } ->
   let open Result.Let_syntax in
-  let%bind x = parameter_id_to_minimal_syntax id in
-  let%map t = type_to_minimal_syntax type_ in
+  let%bind x = simplify_parameter_id id in
+  let%map t = simplify_type type_ in
   x, t
 ;;
 
-let pattern_to_minimal_syntax
-    : Syntax.pattern -> Min.Variable.t Or_static_error.t
+let simplify_pattern : Syntax.pattern -> Min.Variable.t Or_static_error.t
   = function
-  | Syntax.Pattern_id x -> identifier_to_minimal_syntax x |> Result.Ok
+  | Syntax.Pattern_id x -> simplify_identifier x |> Result.Ok
   | Syntax.Pattern_wildcard -> failwith "not implemented"
 ;;
 
-let pattern_parameter_to_minimal_syntax { Syntax.pattern; type_ }
+let simplify_pattern_parameter { Syntax.pattern; type_ }
     : (Min.Variable.t * Type.Mono.t option) Or_static_error.t
   =
   let open Result.Let_syntax in
-  let%bind x = pattern_to_minimal_syntax pattern in
+  let%bind x = simplify_pattern pattern in
   let%map type_' =
-    Option.map type_ ~f:type_to_minimal_syntax
-    |> Static_error.interchange_option
+    Option.map type_ ~f:simplify_type |> Static_error.interchange_option
   in
   x, type_'
 ;;
 
-let operation_parameter_to_minimal_syntax
+let simplify_operation_parameter
     :  Syntax.operation_parameter
     -> (Min.Variable.t * Type.Mono.t option) Or_static_error.t
   =
  fun { Syntax.id; type_ } ->
   let open Result.Let_syntax in
-  let%bind id' = parameter_id_to_minimal_syntax id in
+  let%bind id' = simplify_parameter_id id in
   let%map type_' =
-    Option.map type_ ~f:type_to_minimal_syntax
-    |> Static_error.interchange_option
+    Option.map type_ ~f:simplify_type |> Static_error.interchange_option
   in
   id', type_'
 ;;
 
-let binary_operator_to_minimal_syntax (op : Syntax.binary_operator)
+let simplify_binary_operator (op : Syntax.binary_operator)
     : Min.Operator.t Or_static_error.t
   =
   match op with
@@ -123,65 +118,64 @@ let binary_operator_to_minimal_syntax (op : Syntax.binary_operator)
   | Syntax.Greater_equal -> Result.Ok Min.Operator.(Int Int.Greater_equal)
 ;;
 
-let unary_operator_to_minimal_syntax (op : Syntax.unary_operator)
+let simplify_unary_operator (op : Syntax.unary_operator)
     : Min.Operator.Unary.t Or_static_error.t
   =
   match op with
   | Syntax.Exclamation -> Result.Ok Min.Operator.(Unary.Bool Bool.Unary.Not)
 ;;
 
-let rec expr_to_minimal_syntax (e : Syntax.expr) : Min.Expr.t Or_static_error.t =
+let rec simplify_expr (e : Syntax.expr) : Min.Expr.t Or_static_error.t =
   let open Result.Let_syntax in
   match e with
   | Syntax.Return _ -> Static_error.unsupported_syntax "return" |> Result.Error
   | Syntax.If_then_else (e_cond, block_yes, block_no) ->
-    let%bind e_cond' = expr_to_minimal_syntax e_cond in
-    let%bind block_yes' = block_to_minimal_syntax block_yes in
-    let%map block_no' = block_to_minimal_syntax block_no in
+    let%bind e_cond' = simplify_expr e_cond in
+    let%bind block_yes' = simplify_block block_yes in
+    let%map block_no' = simplify_block block_no in
     Min.Expr.If_then_else (e_cond', block_yes', block_no')
   | Syntax.If_then (e_cond, block_yes) ->
     (* TODO: this is desugaring mixed in *)
     let block_no = Syntax.singleton_block (Syntax.Literal Syntax.Unit) in
-    Syntax.If_then_else (e_cond, block_yes, block_no) |> expr_to_minimal_syntax
+    Syntax.If_then_else (e_cond, block_yes, block_no) |> simplify_expr
   | Syntax.Handler handler ->
-    let%map handler' = effect_handler_to_minimal_syntax handler in
+    let%map handler' = simplify_effect_handler handler in
     Min.Expr.Handler handler'
   | Syntax.Handle { subject; handler } ->
     (* TODO: more desugaring here... *)
-    let%bind handler' = expr_to_minimal_syntax (Syntax.Handler handler) in
-    let%map subject' = expr_to_minimal_syntax subject in
+    let%bind handler' = simplify_expr (Syntax.Handler handler) in
+    let%map subject' = simplify_expr subject in
     Min.Expr.Application (handler', [ subject' ])
-  | Syntax.Fn f -> fn_to_minimal_syntax f
+  | Syntax.Fn f -> simplify_fn f
   | Syntax.Application (e_f, e_args) ->
-    let%bind e_f' = expr_to_minimal_syntax e_f in
-    let%map e_args' = List.map e_args ~f:expr_to_minimal_syntax |> Result.all in
+    let%bind e_f' = simplify_expr e_f in
+    let%map e_args' = List.map e_args ~f:simplify_expr |> Result.all in
     Min.Expr.Application (e_f', e_args')
   | Syntax.Identifier x ->
-    let x' = identifier_to_minimal_syntax x in
+    let x' = simplify_identifier x in
     Min.Expr.Variable x' |> Result.Ok
   | Syntax.Literal lit ->
-    let lit' = literal_to_minimal_syntax lit in
+    let lit' = simplify_literal lit in
     Min.Expr.Literal lit' |> Result.Ok
   | Syntax.Binary_op (e_left, op, e_right) ->
-    let%bind e_left' = expr_to_minimal_syntax e_left in
-    let%bind e_right' = expr_to_minimal_syntax e_right in
-    let%map op' = binary_operator_to_minimal_syntax op in
+    let%bind e_left' = simplify_expr e_left in
+    let%bind e_right' = simplify_expr e_right in
+    let%map op' = simplify_binary_operator op in
     Min.Expr.Operator (e_left', op', e_right')
   | Syntax.Unary_op (op, e) ->
-    let%bind e' = expr_to_minimal_syntax e in
-    let%map op' = unary_operator_to_minimal_syntax op in
+    let%bind e' = simplify_expr e in
+    let%map op' = simplify_unary_operator op in
     Min.Expr.Unary_operator (op', e')
   | Syntax.Annotated (_e, _scheme) ->
     Static_error.unsupported_syntax "type annotation on expression"
     |> Result.Error
 
-and block_to_minimal_syntax { Syntax.statements = _; last = _ }
+and simplify_block { Syntax.statements = _; last = _ }
     : Min.Expr.t Or_static_error.t
   =
   failwith "not implemented"
 
-and fn_to_minimal_syntax
-    { Syntax.type_parameters; parameters; result_type; body }
+and simplify_fn { Syntax.type_parameters; parameters; result_type; body }
     : Min.Expr.t Or_static_error.t
   =
   let open Result.Let_syntax in
@@ -192,7 +186,7 @@ and fn_to_minimal_syntax
     restrict_to_none result_type ~description:"return type annotation"
   in
   let%bind parameters =
-    List.map ~f:pattern_parameter_to_minimal_syntax parameters |> Result.all
+    List.map ~f:simplify_pattern_parameter parameters |> Result.all
   in
   let names, types = List.unzip parameters in
   let%bind () =
@@ -200,15 +194,15 @@ and fn_to_minimal_syntax
       (List.filter_opt types)
       ~description:"type annotations on parameters"
   in
-  let%map body' = block_to_minimal_syntax body in
+  let%map body' = simplify_block body in
   Min.Expr.Lambda (names, body')
 
-and effect_handler_to_minimal_syntax (Syntax.Effect_handler op_handlers)
+and simplify_effect_handler (Syntax.Effect_handler op_handlers)
     : Min.Expr.handler Or_static_error.t
   =
   failwith "not implemented"
 
-and operation_handler_to_minimal_syntax
+and simplify_operation_handler
     :  Syntax.operation_handler
     -> ([ `Op of Min.Variable.t | `Return ] * Min.Expr.op_handler)
        Or_static_error.t
@@ -216,9 +210,9 @@ and operation_handler_to_minimal_syntax
   let open Result.Let_syntax in
   function
   | Syntax.Op_control { id; parameters; body } ->
-    let id' = var_id_to_minimal_syntax id in
+    let id' = simplify_var_id id in
     let%bind parameters' =
-      List.map parameters ~f:operation_parameter_to_minimal_syntax |> Result.all
+      List.map parameters ~f:simplify_operation_parameter |> Result.all
     in
     let names, types = List.unzip parameters' in
     let%bind op_argument =
@@ -231,7 +225,7 @@ and operation_handler_to_minimal_syntax
         (List.filter_opt types)
         ~description:"type annotations on operation handler parameters"
     in
-    let%map op_body = block_to_minimal_syntax body in
+    let%map op_body = simplify_block body in
     `Op id', { Min.Expr.op_argument; op_body }
   (* TODO: this also loses the type safety (shouldn't be able to have a
      `control` handler for a `fun` effect) *)
@@ -242,7 +236,7 @@ and operation_handler_to_minimal_syntax
     in
     let body = { Syntax.statements; last } in
     let as_control = Syntax.Op_control { id; parameters; body } in
-    operation_handler_to_minimal_syntax as_control
+    simplify_operation_handler as_control
   | Syntax.Op_except { id = _; parameters = _; body = _ } ->
     (* TODO: doesn't get anying without proper handling of `resume` and use of
        the knowledge for better implementations *)
@@ -252,20 +246,17 @@ and operation_handler_to_minimal_syntax
        simple, but requires creating a fresh variable name) *)
     Static_error.unsupported_syntax "`val` effect" |> Result.Error
   | Syntax.Op_return { parameter; body } ->
-    let%bind op_argument, type_ =
-      operation_parameter_to_minimal_syntax parameter
-    in
+    let%bind op_argument, type_ = simplify_operation_parameter parameter in
     let%bind () =
       restrict_to_none
         type_
         ~description:"type annotation on operation handler parameter"
     in
-    let%map op_body = block_to_minimal_syntax body in
+    let%map op_body = simplify_block body in
     `Return, { Min.Expr.op_argument; op_body }
 ;;
 
-let operation_declaration_to_minimal_syntax
-    { Syntax.id; type_parameters; shape }
+let simplify_operation_declaration { Syntax.id; type_parameters; shape }
     : (Min.Variable.t * Min.Effect_decl.Operation.t) Or_static_error.t
   =
   let open Result.Let_syntax in
@@ -275,14 +266,14 @@ let operation_declaration_to_minimal_syntax
        (use of `resume`) *)
     Static_error.unsupported_syntax "non `control` effect" |> Result.Error
   | Syntax.Shape_control (parameters, t_answer) ->
-    let id' = var_id_to_minimal_syntax id in
+    let id' = simplify_var_id id in
     let%bind () =
       restrict_to_empty
         type_parameters
         ~description:"type parameters for operation handler"
     in
     let%bind parameters' =
-      List.map parameters ~f:parameter_to_minimal_syntax |> Result.all
+      List.map parameters ~f:simplify_parameter |> Result.all
     in
     let _names, t_parameters = List.unzip parameters' in
     let%bind t_parameter =
@@ -290,17 +281,16 @@ let operation_declaration_to_minimal_syntax
         t_parameters
         ~description:"operation can ony have exactly one argument"
     in
-    let%map t_answer' = type_to_minimal_syntax t_answer in
+    let%map t_answer' = simplify_type t_answer in
     ( id'
     , { Min.Effect_decl.Operation.argument = t_parameter; answer = t_answer' } )
 ;;
 
-let effect_declaration_to_minimal_syntax
-    { Syntax.id; type_parameters; kind; operations }
+let simplify_effect_declaration { Syntax.id; type_parameters; kind; operations }
     : Min.Effect_decl.t Or_static_error.t
   =
   let open Result.Let_syntax in
-  let name = var_id_to_minimal_syntax_effect_label id in
+  let name = simplify_var_id_to_effect_label id in
   let%bind () =
     restrict_to_empty type_parameters ~description:"type parameters for effect"
   in
@@ -309,7 +299,7 @@ let effect_declaration_to_minimal_syntax
     restrict_to_none kind ~description:"kind annotation for effect"
   in
   let%bind operations' =
-    List.map operations ~f:operation_declaration_to_minimal_syntax |> Result.all
+    List.map operations ~f:simplify_operation_declaration |> Result.all
   in
   let%map operations' =
     match Min.Variable.Map.of_alist operations' with
@@ -326,7 +316,7 @@ let effect_declaration_to_minimal_syntax
   { Min.Effect_decl.name; operations = operations' }
 ;;
 
-let program_to_minimal_syntax (_program : Syntax.program)
+let simplify_program (_program : Syntax.program)
     : Min.Program.t Or_static_error.t
   =
   failwith "not implemented"
