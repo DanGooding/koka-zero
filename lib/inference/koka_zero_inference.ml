39e527f1c109261291d9c12697dfda1c66d89d0a
Dan Gooding <dan.gooding2000@gmail.com>
2022-01-03 16:03:29 +0000

    Cleanup inference tests to now work with declarations

--- lib/inference/koka_zero_inference.ml
+++ lib/inference/koka_zero_inference.ml
@@ -3,4 +3,7 @@ module Effect = Effect
 module Minimal_syntax = Minimal_syntax
 
 let check_program = Infer.check_program
-let infer_expr_toplevel = Infer.infer_expr_toplevel
+
+module Private = struct
+  let infer_expr_toplevel = Infer.infer_expr_toplevel
+end
--- lib/inference/koka_zero_inference.mli
+++ lib/inference/koka_zero_inference.mli
@@ -6,9 +6,11 @@ module Minimal_syntax = Minimal_syntax
 (** typecheck a program, returning unit if well typed *)
 val check_program : Minimal_syntax.Program.t -> unit Or_static_error.t
 
-(** infer the type and effect of an expression, in the context containing the
-    given declarations. exists for testing purposes *)
-val infer_expr_toplevel
-  :  Minimal_syntax.Expr.t
-  -> declarations:Minimal_syntax.Decl.t list
-  -> (Type.Mono.t * Effect.t) Or_static_error.t
+module Private : sig
+  (** infer the type and effect of an expression, in the context containing the
+      given declarations. exists for testing purposes *)
+  val infer_expr_toplevel
+    :  Minimal_syntax.Expr.t
+    -> declarations:Minimal_syntax.Decl.t list
+    -> (Type.Mono.t * Effect.t) Or_static_error.t
+end
