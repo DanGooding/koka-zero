fc7f603e4c3e0a9a4837ba11fa0e20bf87fc30fe
Dan Gooding <dan.gooding2000@gmail.com>
2022-01-09 21:59:52 +0000

    Implement evaluation of match-ctl expressions

--- lib/interpreter/typecast.mli
+++ lib/interpreter/typecast.mli
@@ -1,4 +1,10 @@
+val ctl_of_value : Value.t -> Value.ctl Interpreter.t
 val closure_of_value : Value.t -> Value.closure Interpreter.t
 val marker_of_value : Value.t -> Value.Marker.t Interpreter.t
 val bool_of_value : Value.t -> bool Interpreter.t
 val int_of_value : Value.t -> int Interpreter.t
+
+val zip_arguments
+  :  params:'a list
+  -> args:'b list
+  -> ('a * 'b) list Interpreter.t
