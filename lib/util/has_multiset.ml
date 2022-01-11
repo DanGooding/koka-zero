bd7ec5b8df70c847f48bf693eb34e6c08edb46ca
Dan Gooding <dan.gooding2000@gmail.com>
2022-01-11 17:28:23 +0000

    Implement non-empty multisets

---
+++ lib/util/has_multiset.ml
@@ -0,0 +1,104 @@
+open Core
+include Has_multiset_intf
+
+module Make (Element : sig
+  type t [@@deriving sexp]
+
+  include Comparable.S with type t := t
+end) =
+struct
+  module Multiset = struct
+    module Multiset = struct
+      module Element = Element
+
+      (* invariant: all entries have value (count) >= 1 *)
+      type t = int Element.Map.t [@@deriving sexp]
+
+      (** restore the invariant that only items 'in' the multiset are keys in
+          the map *)
+      let remove_zeros xs = Element.Map.filter xs ~f:(fun n -> n > 0)
+
+      let of_list xs =
+        List.map xs ~f:(fun x -> x, 1)
+        |> Element.Map.of_alist_reduce ~f:Int.( + )
+      ;;
+
+      let to_list xs =
+        Element.Map.fold xs ~init:[] ~f:(fun ~key:element ~data:n acc ->
+            List.init n ~f:(fun _i -> element) @ acc)
+      ;;
+
+      let empty = Element.Map.empty
+
+      let add xs x =
+        Element.Map.update xs x ~f:(fun n -> Option.value n ~default:0 + 1)
+      ;;
+
+      let union xs ys =
+        Map.merge xs ys ~f:(fun ~key:_ data ->
+            match data with
+            | `Left n | `Right n -> Some n
+            | `Both (m, n) -> Some (m + n))
+      ;;
+
+      let inter xs ys =
+        Map.merge xs ys ~f:(fun ~key:_ data ->
+            match data with
+            | `Left _ | `Right _ -> None
+            | `Both (m, n) -> Some (min m n))
+      ;;
+
+      let diff xs ys =
+        Map.mapi xs ~f:(fun ~key:element ~data:nx ->
+            let ny = Element.Map.find ys element |> Option.value ~default:0 in
+            max (nx - ny) 0)
+        |> remove_zeros
+      ;;
+
+      let is_empty xs = Element.Map.is_empty xs
+    end
+
+    module Non_empty = struct
+      module T = struct
+        type t = Non_empty of Multiset.t [@@deriving sexp]
+      end (* disable "fragile-match" for generated code *) [@warning "-4"]
+
+      include T
+
+      let of_multiset m =
+        if Multiset.is_empty m then None else Some (Non_empty m)
+      ;;
+
+      let of_multiset_verbose m =
+        match of_multiset m with
+        | None -> `Empty
+        | Some m -> `Non_empty m
+      ;;
+
+      let of_multiset_exn m =
+        match of_multiset m with
+        | Some t -> t
+        | None -> raise_s [%message "got empty multiset"]
+      ;;
+
+      let of_list xs = Multiset.of_list xs |> of_multiset
+      let of_list_exn xs = Multiset.of_list xs |> of_multiset_exn
+      let to_list (Non_empty m) = Multiset.to_list m
+      let to_multiset (Non_empty m) = m
+      let add (Non_empty m) x = Multiset.add m x |> Non_empty
+
+      let union (Non_empty m1) (Non_empty m2) =
+        Multiset.union m1 m2 |> Non_empty
+      ;;
+
+      let inter_to_multiset (Non_empty m1) (Non_empty m2) = Multiset.inter m1 m2
+      let diff_to_multiset (Non_empty m1) (Non_empty m2) = Multiset.diff m1 m2
+      let inter t1 t2 = inter_to_multiset t1 t2 |> of_multiset_verbose
+      let diff t1 t2 = diff_to_multiset t1 t2 |> of_multiset_verbose
+      let inter_opt t1 t2 = inter_to_multiset t1 t2 |> of_multiset
+      let diff_opt t1 t2 = diff_to_multiset t1 t2 |> of_multiset
+    end
+
+    include Multiset
+  end
+end
--- test/util/test_multiset.ml
+++ test/util/test_multiset.ml
@@ -6,18 +6,20 @@ module Name : sig
 
   val t_of_string : string -> t
 
-  module Multiset : sig
-    include Multiset.S with type Element.t := t
-  end
+  include Has_multiset.S with type t := t
 end = struct
   include String
 
   let t_of_string x = x
 
-  module Multiset = Multiset.Make (String)
+  include Has_multiset.Make (String)
 end
 
 let of_strings xs = List.map xs ~f:Name.t_of_string |> Name.Multiset.of_list
+
+let non_empty_of_strings_exn xs =
+  List.map xs ~f:Name.t_of_string |> Name.Multiset.Non_empty.of_list_exn
+;;
 
 let print_multiset names =
   Name.Multiset.sexp_of_t names |> Sexp.to_string_hum |> print_endline
@@ -48,4 +50,21 @@ let%expect_test "set operations work correctly" =
   [%expect {| ((a 3) (b 1) (e 1)) |}];
   Name.Multiset.inter x y |> print_multiset;
   [%expect {| ((a 1) (b 1) (c 1)) |}]
+;;
+
+let%expect_test "cannot create empty non_empty_multiset" =
+  let x = of_strings [] |> Name.Multiset.Non_empty.of_multiset in
+  [%sexp (x : Name.Multiset.Non_empty.t option)] |> print_s;
+  [%expect {| () |}];
+  let y = non_empty_of_strings_exn [ "a"; "a"; "b" ] in
+  let z = non_empty_of_strings_exn [ "c"; "d"; "e" ] in
+  Name.Multiset.Non_empty.inter_opt y z
+  |> [%sexp_of: Name.Multiset.Non_empty.t option]
+  |> print_s;
+  [%expect {| () |}];
+  let w = non_empty_of_strings_exn [ "a"; "a"; "a"; "b"; "b"; "c" ] in
+  Name.Multiset.Non_empty.diff_opt y w
+  |> [%sexp_of: Name.Multiset.Non_empty.t option]
+  |> print_s;
+  [%expect {| ()  |}]
 ;;
