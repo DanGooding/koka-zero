81c46e94ebe8f163acd783c144d2ca29c6f4beb0
Dan Gooding <dan.gooding2000@gmail.com>
2022-01-18 22:11:43 +0000

    Implement most parts of compiling functions
    Lacking:
    - unique symbol names
    - memoizing escaping closure (keep internally in context)

--- lib/code-generation/context.mli
+++ lib/code-generation/context.mli
@@ -3,17 +3,21 @@ open Import
 module Parameters : sig
   (** maps the current function's parameters to their [llvalues] *)
   type t = (Variable.t * Llvm.llvalue) list
-  (* TODO: does this contain the extraneous `self` parameter for non recursive
-     functions? *)
 
   val find : t -> Variable.t -> Llvm.llvalue option
 end
 
 module Closure : sig
   module Shape : sig
+    (** describes the statically known shape and contents of a closure *)
     type t =
       | Level of Variable.t list * t
-      | Empty
+          (** a list of all the variables added to the closure at one nesting
+              depth, and the parent closure's shape *)
+      | Toplevel of int Variable.Map.t
+          (** a map from names in scope from the current enclosing toplevel
+              function, to their indices in the _single_ toplevel closure. This
+              is is map rather than a list due to potential shadowing *)
     [@@deriving sexp]
   end
 
@@ -23,8 +27,8 @@ module Closure : sig
     ; shape : Shape.t
     }
 
-  (** cons parameters on the front, producing a child closure *)
-  val extend : t -> Parameters.t -> runtime:Runtime.t -> t Codegen.t
+  (** produce code to cons parameters on the front, producing a child closure *)
+  val compile_extend : t -> Parameters.t -> runtime:Runtime.t -> t Codegen.t
 
   (** generate code to retrieve a varaible from a closure, failing with a
       codegen_eror if it is not present. *)
@@ -36,6 +40,9 @@ type t =
   { parameters : Parameters.t
   ; closure : Closure.t
   }
+
+(** generate code to extend the closure with the current parameters *)
+val compile_make_closure : t -> runtime:Runtime.t -> Closure.t Codegen.t
 
 (** generate code to retrieve an in-scope variable, either directly from
     [parameters], or indirectly in the [closure]. fails with a codegen_error if
