dec5b2d9e352defc7ffa98591dec882212553c47
Dan Gooding <dan.gooding2000@gmail.com>
2022-01-25 20:43:42 +0000

    Expose compilation in koka zero

---
+++ lib/code-generation/koka_zero_code_generation.mli
@@ -0,0 +1,16 @@
+open Import
+open Koka_zero_evidence_translation
+module Codegen_error = Codegen_error
+
+(** compile a program, then write the resulting module as textual `.ll` code to
+    a file *)
+val compile_then_write_program
+  :  Evidence_passing_syntax.Program.t
+  -> filename:string
+  -> unit Or_codegen_error.t
+
+(** compile a program, then write the resulting module as textual `.ll` to
+    stderr *)
+val compile_then_dump_program
+  :  Evidence_passing_syntax.Program.t
+  -> unit Or_codegen_error.t
