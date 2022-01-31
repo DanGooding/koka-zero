4ecaa7d81a8915d8fe21dabc10ba4d8812a7183f
Dan Gooding <dan.gooding2000@gmail.com>
2022-01-31 20:15:39 +0000

    Add compiler driver script

---
+++ compile.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+KC=./_build/default/bin/main.exe
+CC=clang
+CFLAGS=-Wall
+
+RUNTIME=lib/code-generation/runtime.c
+
+SOURCE="$1"
+BASENAME="${SOURCE%.kk}"
+
+IR="$BASENAME.ll"
+
+BINARY="$BASENAME"
+
+# try to avoid overwriting the source file
+if [[ "$BINARY" = "$SOURCE" ]]; then
+    BINARY="$BINARY.exe"
+fi
+
+$KC compile $SOURCE -o $IR
+$CC $CFLAGS $IR $RUNTIME -o $BINARY
+
