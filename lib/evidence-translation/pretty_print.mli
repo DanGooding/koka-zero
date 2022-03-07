d6b3bdf01f8daf4d9ba54d03144d7079a7e58f0d
Dan Gooding <dan.gooding2000@gmail.com>
2022-03-07 11:41:37 +0000

    Clarify pretty print documentation

--- lib/evidence-translation/pretty_print.mli
+++ lib/evidence-translation/pretty_print.mli
@@ -1,7 +1,8 @@
 open Evidence_passing_syntax
 
-(** print an expression to equivalent OCaml code (which can then be run through
-    ocamlformat) *)
+(** print an expression to equivalent OCaml code, which can then be run through
+    ocamlformat for inspection. The produced code will not typecheck and is not
+    intended for execution *)
 val print_expr : Expr.t -> string
 
 val print_program : Program.t -> string
