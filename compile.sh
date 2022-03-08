f8ca4f48202fd8c9adf8c4d0006370aaa0dec580
Dan Gooding <dan.gooding2000@gmail.com>
2022-03-08 15:37:05 +0000

    Disable GC when compile via environment variable

--- compile.sh
+++ compile.sh
@@ -11,10 +11,16 @@ KOKA_COMPILER=${KOKA_COMPILER:-$PROJECT_ROOT/_build/default/bin/main.exe}
 LL_C_COMPILER=clang
 OPT_LEVEL=${OPT_LEVEL:-0}
 # intentionally rely on clang to know the system's target triple
-LL_C_FLAGS="-Wall -Wno-override-module -DENABLE_GC -O$OPT_LEVEL"
+LL_C_FLAGS="-Wall -Wno-override-module -O$OPT_LEVEL"
 
 RUNTIME=$PROJECT_ROOT/lib/runtime/runtime.c
-GC=/home/dan/boehm/gc
+
+# set DISABLE_GC to run without any deallocator (e.g. for debugging)
+if [ -z "$DISABLE_GC" ]; then
+    # GC enabled (default)
+    GC=/home/dan/boehm/gc
+    LL_C_FLAGS="$LL_C_FLAGS -DENABLE_GC -I$GC/include -L$GC/lib -lgc"
+fi
 
 SOURCE="$1"
 BASENAME="${SOURCE%.kk}"
@@ -29,5 +35,5 @@ if [[ "$BINARY" = "$SOURCE" ]]; then
 fi
 
 $KOKA_COMPILER compile $SOURCE -o $IR
-$LL_C_COMPILER $LL_C_FLAGS $IR $RUNTIME -I$GC/include -L$GC/lib -lgc -o $BINARY
+$LL_C_COMPILER $LL_C_FLAGS $IR $RUNTIME -o $BINARY
 
