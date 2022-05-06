0c239014fd43512fa0bf513a7ceb621a37ce8c80
Dan Gooding <dan.gooding2000@gmail.com>
2022-05-06 19:11:16 +0100

    Fix change of translation exports

--- lib/evidence-translation/koka_zero_evidence_translation.ml
+++ lib/evidence-translation/koka_zero_evidence_translation.ml
@@ -6,7 +6,7 @@ let rewrite_program = Rewriting.rewrite_program
 module Free_variables = Free_variables
 
 module Private = struct
-  let translate_expr expr = Generation.run (Translation.translate_expr expr)
+  let translate_lambda expr = Generation.run (Translation.translate_lambda expr)
 
   let translate_no_prelude program =
     Generation.run (Translation.translate_no_prelude program)
