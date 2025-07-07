7200c2728bc84a7e02241016dc21693b56e149bd
Dan Gooding <dan.gooding2000@gmail.com>
2025-07-07 13:33:43 +0100

    Change bounds naming to camelcase

--- lib/phases/02-subtyping-inference/bounds.mli
+++ lib/phases/02-subtyping-inference/bounds.mli
@@ -1,11 +1,11 @@
 open! Core
 open! Import
 
-(** encodes a set of constraints of type [lowerBound <= _] and [_ <= upperBound] **)
+(** encodes a set of constraints of type [lower_bound <= _] and [_ <= upper_bound] **)
 type 'a t =
   { (* TODO: should these be hash_sets? *)
-    mutable lowerBounds : 'a list
-  ; mutable upperBounds : 'a list
+    mutable lower_bounds : 'a list
+  ; mutable upper_bounds : 'a list
   }
 [@@deriving sexp_of]
 
