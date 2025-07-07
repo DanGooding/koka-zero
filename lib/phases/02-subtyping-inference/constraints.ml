719a56932c9f6542d809a63d6006a9c2f48eba06
Dan Gooding <dan.gooding2000@gmail.com>
2025-07-07 14:09:36 +0100

    Convert infer to result monad

--- lib/phases/02-subtyping-inference/constraints.ml
+++ lib/phases/02-subtyping-inference/constraints.ml
@@ -185,23 +185,34 @@ let extrude_effect t effect_ ~to_level ~polarity_positive =
 
 (** add constraints for [type_lo <= type_hi]
     fails if we find something unsatisfiable - i.e. a type error *)
-let rec constrain_type_at_most_exn
-          t
-          (type_lo : Type.Mono.t)
-          (type_hi : Type.Mono.t)
+let rec constrain_type_at_most t (type_lo : Type.Mono.t) (type_hi : Type.Mono.t)
+  : unit Or_error.t
   =
+  let open Result.Let_syntax in
   let constraint_ = Constraint.Type_at_most { type_lo; type_hi } in
   match Hash_set.strict_add t.already_seen_constraints constraint_ with
-  | Error _already_present -> ()
+  | Error _already_present -> return ()
   | Ok () ->
     (match type_lo, type_hi with
      | ( Arrow (args_lo, effect_lo, result_lo)
        , Arrow (args_hi, effect_hi, result_hi) ) ->
-       List.iter2_exn args_lo args_hi ~f:(fun arg_lo arg_hi ->
-         constrain_type_at_most_exn t arg_hi arg_lo);
-       constrain_effect_at_most_exn t effect_lo effect_hi;
-       constrain_type_at_most_exn t result_lo result_hi
-     | Primitive p, Primitive p' when [%equal: Type.Primitive.t] p p' -> ()
+       let%bind () =
+         match
+           List.map2 args_lo args_hi ~f:(fun arg_lo arg_hi ->
+             constrain_type_at_most t arg_hi arg_lo)
+         with
+         | Ok results -> Result.all_unit results
+         | Unequal_lengths ->
+           Or_error.error_s
+             [%message
+               "function type has wrong number of arguments"
+                 (args_lo : Type.Mono.t list)
+                 (args_hi : Type.Mono.t list)]
+       in
+       let%bind () = constrain_effect_at_most t effect_lo effect_hi in
+       constrain_type_at_most t result_lo result_hi
+     | Primitive p, Primitive p' when [%equal: Type.Primitive.t] p p' ->
+       return ()
      | Primitive p, Primitive p' ->
        raise_s
          [%message
@@ -224,16 +235,17 @@ let rec constrain_type_at_most_exn
           (* add [m <= type_hi]*)
           m_bounds.upper_bounds <- type_hi :: m_bounds.upper_bounds;
           (* add transitive closure *)
-          List.iter m_bounds.lower_bounds ~f:(fun below_m ->
-            constrain_type_at_most_exn t below_m type_hi)
+          List.map m_bounds.lower_bounds ~f:(fun below_m ->
+            constrain_type_at_most t below_m type_hi)
+          |> Result.all_unit
         | false ->
           (* need to copy [type_hi] down to [m_level] *)
           let approx_type_hi =
             extrude t type_hi ~to_level:m_level ~polarity_positive:false
           in
           (* m (level_m) <= approx_type_hi (level_m) <= type_hi *)
-          constrain_type_at_most_exn t approx_type_hi type_hi;
-          constrain_type_at_most_exn t (Metavariable m) approx_type_hi)
+          let%bind () = constrain_type_at_most t approx_type_hi type_hi in
+          constrain_type_at_most t (Metavariable m) approx_type_hi)
      | type_lo, Metavariable m ->
        let m_level = Metavariables.type_level_exn t.metavariables m in
        let lo_level =
@@ -252,14 +264,15 @@ let rec constrain_type_at_most_exn
           (* add [type_less_than_m <= m] *)
           m_bounds.lower_bounds <- type_lo :: m_bounds.lower_bounds;
           (* add transitive closure *)
-          List.iter m_bounds.upper_bounds ~f:(fun above_m ->
-            constrain_type_at_most_exn t type_lo above_m)
+          List.map m_bounds.upper_bounds ~f:(fun above_m ->
+            constrain_type_at_most t type_lo above_m)
+          |> Result.all_unit
         | false ->
           let approx_type_lo =
             extrude t type_lo ~to_level:m_level ~polarity_positive:true
           in
-          constrain_type_at_most_exn t type_lo approx_type_lo;
-          constrain_type_at_most_exn t approx_type_lo (Metavariable m))
+          let%bind () = constrain_type_at_most t type_lo approx_type_lo in
+          constrain_type_at_most t approx_type_lo (Metavariable m))
      | Variable v, _ | _, Variable v ->
        raise_s [%message "unexpected type variable" (v : Type.Variable.t)]
      | Arrow _, Primitive _ | Primitive _, Arrow _ ->
@@ -269,24 +282,26 @@ let rec constrain_type_at_most_exn
              (type_lo : Type.Mono.t)
              (type_hi : Type.Mono.t)])
 
-and constrain_effect_at_most_exn t (effect_lo : Effect.t) (effect_hi : Effect.t)
+and constrain_effect_at_most t (effect_lo : Effect.t) (effect_hi : Effect.t)
+  : unit Or_error.t
   =
+  let open Result.Let_syntax in
   let constraint_ = Constraint.Effect_at_most { effect_lo; effect_hi } in
   match Hash_set.strict_add t.already_seen_constraints constraint_ with
-  | Error _already_present -> ()
+  | Error _already_present -> return ()
   | Ok () ->
     (match effect_lo, effect_hi with
      | Labels labels_lo, Labels labels_hi
-       when Set.is_subset labels_lo ~of_:labels_hi -> ()
+       when Set.is_subset labels_lo ~of_:labels_hi -> return ()
      | Labels labels_lo, Labels labels_hi ->
-       raise_s
+       Or_error.error_s
          [%message
            "less-than constraint doesn't hold"
              ~labels:(labels_lo : Effect.Label.Set.t)
              ~expected_at_most:(labels_hi : Effect.Label.Set.t)]
      | Handled (labels_lo, effect_lo), Labels labels_hi ->
        (* effect_lo - labels_lo <= labels_hi *)
-       constrain_effect_at_most_exn
+       constrain_effect_at_most
          t
          (Unknown effect_lo)
          (Labels (Set.union labels_lo labels_hi))
@@ -296,7 +311,7 @@ and constrain_effect_at_most_exn t (effect_lo : Effect.t) (effect_hi : Effect.t)
         | false ->
           (* we're requiring that a row containing L is <= a row not containing L
              for L being the intersection. This can never hold. *)
-          raise_s
+          Or_error.error_s
             [%message
               "less-than constraint doesn't hold"
                 ~labels:(labels_lo : Effect.Label.Set.t)
@@ -304,25 +319,27 @@ and constrain_effect_at_most_exn t (effect_lo : Effect.t) (effect_hi : Effect.t)
         | true ->
           (* it's optional whether effect_hi contains labels' or not.
              so the constraint reduces to labels_lo <= effect_hi *)
-          constrain_effect_at_most_exn t (Labels labels_lo) (Unknown effect_hi))
+          constrain_effect_at_most t (Labels labels_lo) (Unknown effect_hi))
      | Handled (labels_lo, effect_lo), Handled (labels_hi, effect_hi) ->
        (* effect_lo - labels_lo <= effect_hi - labels_hi
         we reduce into multiple simpler but equiavalent constraints to make progress *)
        (* weaken - increase the upper bound, getting:
          effect_lo - labels_lo <= effect_hi *)
-       constrain_effect_at_most_exn
-         t
-         (Handled (labels_lo, effect_lo))
-         (Unknown effect_hi);
-       (* add back the info that [effect_lo] does not contain any of [labels_hi], 
+       let%bind () =
+         constrain_effect_at_most
+           t
+           (Handled (labels_lo, effect_lo))
+           (Unknown effect_hi)
+         (* add back the info that [effect_lo] does not contain any of [labels_hi], 
          except possibly those in [labels_lo]:
          effect_lo <= effect_lo - (labels_hi - labels_lo) *)
-       (* TODO: can we actually make use of this constraint? 
+         (* TODO: can we actually make use of this constraint? 
          perhaps instead, for each upper bound effect_lo <= above
          add: effect_lo <= Handled(diff, above)
          (we'll want to add this for subsequently added bounds too)
       *)
-       constrain_effect_at_most_exn
+       in
+       constrain_effect_at_most
          t
          (Unknown effect_lo)
          (Handled (Set.diff labels_hi labels_lo, effect_lo))
@@ -339,14 +356,15 @@ and constrain_effect_at_most_exn t (effect_lo : Effect.t) (effect_hi : Effect.t)
             Hashtbl.find_or_add t.effect_constraints m ~default:Bounds.create
           in
           m_bounds.upper_bounds <- above_m :: m_bounds.upper_bounds;
-          List.iter m_bounds.lower_bounds ~f:(fun below_m ->
-            constrain_effect_at_most_exn t below_m above_m)
+          List.map m_bounds.lower_bounds ~f:(fun below_m ->
+            constrain_effect_at_most t below_m above_m)
+          |> Result.all_unit
         | false ->
           let above_approx =
             extrude_effect t above_m ~to_level:m_level ~polarity_positive:false
           in
-          constrain_effect_at_most_exn t above_approx above_m;
-          constrain_effect_at_most_exn t (Unknown (Metavariable m)) above_approx)
+          let%bind () = constrain_effect_at_most t above_approx above_m in
+          constrain_effect_at_most t (Unknown (Metavariable m)) above_approx)
      | below_m, Unknown (Metavariable m) ->
        let m_level = Metavariables.effect_level_exn t.metavariables m in
        let below_m_level =
@@ -360,19 +378,17 @@ and constrain_effect_at_most_exn t (effect_lo : Effect.t) (effect_hi : Effect.t)
             Hashtbl.find_or_add t.effect_constraints m ~default:Bounds.create
           in
           m_bounds.lower_bounds <- below_m :: m_bounds.lower_bounds;
-          List.iter m_bounds.upper_bounds ~f:(fun above_m ->
-            constrain_effect_at_most_exn t below_m above_m)
+          List.map m_bounds.upper_bounds ~f:(fun above_m ->
+            constrain_effect_at_most t below_m above_m)
+          |> Result.all_unit
         | false ->
           let approx_below_m =
             extrude_effect t below_m ~to_level:m_level ~polarity_positive:true
           in
-          constrain_effect_at_most_exn t below_m approx_below_m;
-          constrain_effect_at_most_exn
-            t
-            approx_below_m
-            (Unknown (Metavariable m)))
+          let%bind () = constrain_effect_at_most t below_m approx_below_m in
+          constrain_effect_at_most t approx_below_m (Unknown (Metavariable m)))
      | Unknown (Variable v), _ | _, Unknown (Variable v) ->
-       raise_s
+       Or_error.error_s
          [%message
            "unexpected effect variable in constraint" (v : Effect.Variable.t)])
 ;;
