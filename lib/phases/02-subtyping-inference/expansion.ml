a847323e3d8027218fb89f7c4dc8cdb2bf4c90ca
Dan Gooding <dan.gooding2000@gmail.com>
2025-07-07 08:56:03 +0100

    Fix type metavariable expansion

--- lib/phases/02-subtyping-inference/expansion.ml
+++ lib/phases/02-subtyping-inference/expansion.ml
@@ -3,25 +3,28 @@ open! Import
 
 type t =
   { constraints : Constraints.t
-  ; in_progress_type_metavariables : Type.Variable.t Type.Metavariable.Table.t
-  ; in_progress_effect_metavariables :
-      Effect.Variable.t Effect.Metavariable.Table.t
+  ; type_meta_to_var : Type.Variable.t Type.Metavariable.Table.t
+  ; effect_meta_to_var : Effect.Variable.t Effect.Metavariable.Table.t
   ; type_variable_source : Type.Variable.Name_source.t
   ; effect_variable_source : Effect.Variable.Name_source.t
   }
 
 let create ~constraints ~type_variable_source ~effect_variable_source =
-  let in_progress_type_metavariables = Type.Metavariable.Table.create () in
-  let in_progress_effect_metavariables = Effect.Metavariable.Table.create () in
+  let type_meta_to_var = Type.Metavariable.Table.create () in
+  let effect_meta_to_var = Effect.Metavariable.Table.create () in
   { constraints
   ; type_variable_source
   ; effect_variable_source
-  ; in_progress_type_metavariables
-  ; in_progress_effect_metavariables
+  ; type_meta_to_var
+  ; effect_meta_to_var
   }
 ;;
 
-let rec expand_type_aux t (type_ : Type.Mono.t) ~polarity_positive
+let rec expand_type_aux
+          t
+          (type_ : Type.Mono.t)
+          ~polarity_positive
+          ~(in_progress_type_metavariables : Type.Metavariable.Set.t)
   : Polar_type.t
   =
   match type_ with
@@ -30,22 +33,40 @@ let rec expand_type_aux t (type_ : Type.Mono.t) ~polarity_positive
     let args =
       List.map
         args
-        ~f:(expand_type_aux t ~polarity_positive:(not polarity_positive))
+        ~f:
+          (expand_type_aux
+             t
+             ~polarity_positive:(not polarity_positive)
+             ~in_progress_type_metavariables)
     in
-    let effect_ = expand_effect_aux t effect_ ~polarity_positive in
-    let result = expand_type_aux t result ~polarity_positive in
+    let effect_ =
+      expand_effect_aux
+        t
+        effect_
+        ~polarity_positive
+        ~in_progress_effect_metavariables:Effect.Metavariable.Set.empty
+    in
+    let result =
+      expand_type_aux
+        t
+        result
+        ~polarity_positive
+        ~in_progress_type_metavariables
+    in
     Arrow (args, effect_, result)
   | Variable _ ->
     (* unclear if we'll encounter this once polymorphism is added *)
     raise_s [%message "unexpected variable when expanding type"]
   | Metavariable meta ->
-    (match Hashtbl.find t.in_progress_type_metavariables meta with
-     | Some var ->
+    let var =
+      Hashtbl.find_or_add t.type_meta_to_var meta ~default:(fun () ->
+        Type.Variable.Name_source.next_name t.type_variable_source)
+    in
+    (match Set.mem in_progress_type_metavariables meta with
+     | true ->
        (* recursive reference - leave unexpanded, it'll be bound in a Recursive type *)
        Variable var
-     | None ->
-       let var = Type.Variable.Name_source.next_name t.type_variable_source in
-       Hashtbl.add_exn t.in_progress_type_metavariables ~key:meta ~data:var;
+     | false ->
        let bounds =
          Constraints.get_type_bounds t.constraints meta
          |> Option.value_map
@@ -57,7 +78,12 @@ let rec expand_type_aux t (type_ : Type.Mono.t) ~polarity_positive
        in
        let bound_types =
          List.map bounds ~f:(fun bound_type ->
-           expand_type_aux t bound_type ~polarity_positive)
+           expand_type_aux
+             t
+             bound_type
+             ~polarity_positive
+             ~in_progress_type_metavariables:
+               (Set.add in_progress_type_metavariables meta))
        in
        let is_recursive =
          List.exists bound_types ~f:(fun type_ ->
@@ -75,17 +101,36 @@ let rec expand_type_aux t (type_ : Type.Mono.t) ~polarity_positive
            | [] -> Variable var
            | _ :: _ -> combined)))
 
-and expand_effect_aux t (effect_ : Effect.t) ~polarity_positive
+and expand_effect_aux
+      t
+      (effect_ : Effect.t)
+      ~polarity_positive
+      ~(in_progress_effect_metavariables : Effect.Metavariable.Set.t)
   : Polar_type.Effect.t
   =
   match effect_ with
   | Labels labels -> Labels labels
   | Handled (labels, unknown) ->
-    let effect_ = expand_unknown_effect_aux t unknown ~polarity_positive in
+    let effect_ =
+      expand_unknown_effect_aux
+        t
+        unknown
+        ~polarity_positive
+        ~in_progress_effect_metavariables
+    in
     Handled (labels, effect_)
-  | Unknown unknown -> expand_unknown_effect_aux t unknown ~polarity_positive
+  | Unknown unknown ->
+    expand_unknown_effect_aux
+      t
+      unknown
+      ~polarity_positive
+      ~in_progress_effect_metavariables
 
-and expand_unknown_effect_aux t (effect_ : Effect.Unknown.t) ~polarity_positive
+and expand_unknown_effect_aux
+      t
+      (effect_ : Effect.Unknown.t)
+      ~polarity_positive
+      ~in_progress_effect_metavariables
   : Polar_type.Effect.t
   =
   match effect_ with
@@ -93,15 +138,15 @@ and expand_unknown_effect_aux t (effect_ : Effect.Unknown.t) ~polarity_positive
     raise_s [%message "unexpected effect variable when expanding type"]
   | Metavariable meta ->
     (* TODO: avoid duplication with the type case *)
-    (match Hashtbl.find t.in_progress_effect_metavariables meta with
-     | Some var ->
+    let var =
+      Hashtbl.find_or_add t.effect_meta_to_var meta ~default:(fun () ->
+        Effect.Variable.Name_source.next_name t.effect_variable_source)
+    in
+    (match Set.mem in_progress_effect_metavariables meta with
+     | true ->
        (* recursive reference - leave unexpanded, it'll be bound in a Recursive type *)
        Variable var
-     | None ->
-       let var =
-         Effect.Variable.Name_source.next_name t.effect_variable_source
-       in
-       Hashtbl.add_exn t.in_progress_effect_metavariables ~key:meta ~data:var;
+     | false ->
        let bounds =
          Constraints.get_effect_bounds t.constraints meta
          |> Option.value_map
@@ -113,7 +158,12 @@ and expand_unknown_effect_aux t (effect_ : Effect.Unknown.t) ~polarity_positive
        in
        let bound_effects =
          List.map bounds ~f:(fun bound_effect ->
-           expand_effect_aux t bound_effect ~polarity_positive)
+           expand_effect_aux
+             t
+             bound_effect
+             ~polarity_positive
+             ~in_progress_effect_metavariables:
+               (Set.add in_progress_effect_metavariables meta))
        in
        let is_recursive =
          List.exists bound_effects ~f:(fun type_ ->
@@ -132,8 +182,18 @@ and expand_unknown_effect_aux t (effect_ : Effect.Unknown.t) ~polarity_positive
            | _ :: _ -> combined)))
 ;;
 
-let expand_type t type_ = expand_type_aux t type_ ~polarity_positive:true
+let expand_type t type_ =
+  expand_type_aux
+    t
+    type_
+    ~polarity_positive:true
+    ~in_progress_type_metavariables:Type.Metavariable.Set.empty
+;;
 
 let expand_effect t effect_ =
-  expand_effect_aux t effect_ ~polarity_positive:true
+  expand_effect_aux
+    t
+    effect_
+    ~polarity_positive:true
+    ~in_progress_effect_metavariables:Effect.Metavariable.Set.empty
 ;;
