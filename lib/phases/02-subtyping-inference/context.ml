b88e8f51f5775995807f902eba1f15dd864b6bbc
Dan Gooding <dan.gooding2000@gmail.com>
2025-07-07 13:47:58 +0100

    Switch new inference to use old context

--- lib/phases/02-subtyping-inference/context.ml
+++ lib/phases/02-subtyping-inference/context.ml
@@ -1,8 +1,67 @@
 open! Core
 open! Import
 
-type t = Type.t Variable.Map.t [@@deriving sexp_of]
+module Binding = struct
+  type t =
+    | Value of Type.t
+    | Operation of Effect.Label.t * Type.t
+  [@@deriving sexp_of]
+end
+
+module Entry = struct
+  type t =
+    { binding : Binding.t
+    ; shadowable : bool
+    }
+  [@@deriving sexp_of]
+end
+
+module Or_cannot_shadow = struct
+  type 'a t =
+    [ `Ok of 'a
+    | `Cannot_shadow
+    ]
+  [@@deriving sexp_of]
+end
+
+type t = Entry.t Variable.Map.t [@@deriving sexp_of]
+
+let can_bind t var =
+  match Map.find t var with
+  | Some { Entry.shadowable; _ } -> shadowable
+  | None -> true
+;;
+
+let extend t ~var ~type_ =
+  if can_bind t var
+  then (
+    let binding = Binding.Value type_ in
+    let entry = { Entry.shadowable = true; binding } in
+    `Ok (Map.set t ~key:var ~data:entry))
+  else `Cannot_shadow
+;;
+
+let extend_toplevel t ~var ~type_ =
+  let binding = Binding.Value type_ in
+  let entry = { Entry.shadowable = true; binding } in
+  match Map.add t ~key:var ~data:entry with
+  | `Duplicate -> `Cannot_shadow
+  | `Ok t' -> `Ok t'
+;;
+
+let extend_operation t ~var ~label ~type_ =
+  if Map.mem t var
+  then `Cannot_shadow
+  else (
+    let binding = Binding.Operation (label, type_) in
+    let entry = { Entry.shadowable = false; binding } in
+    `Ok (Map.set t ~key:var ~data:entry))
+;;
+
+let find t var =
+  match Map.find t var with
+  | None -> None
+  | Some { Entry.binding; _ } -> Some binding
+;;
 
 let empty = Variable.Map.empty
-let extend t ~name ~type_ = Map.set t ~key:name ~data:type_
-let get_exn t name = Map.find_exn t name
