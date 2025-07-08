85954c708be87c6737351cb491d8c4ea53219003
Dan Gooding <dan.gooding2000@gmail.com>
2025-07-08 17:06:04 +0100

    Make infer.t local only

--- lib/phases/02-subtyping-inference/infer.mli
+++ lib/phases/02-subtyping-inference/infer.mli
@@ -1,22 +1,15 @@
 open! Core
 open! Import
 
-type t
-
-val create : unit -> t
-
 val infer_expr_toplevel
-  :  t
-  -> Minimal_syntax.Expr.t
+  :  Minimal_syntax.Expr.t
   -> declarations:Minimal_syntax.Decl.t list
   -> (Polar_type.t * Polar_type.Effect.t * Explicit_syntax.Expr.t) Or_error.t
 
 val infer_program
-  :  t
-  -> Minimal_syntax.Program.t
+  :  Minimal_syntax.Program.t
   -> Explicit_syntax.Program.t Or_error.t
 
 val infer_program_without_main
-  :  t
-  -> Minimal_syntax.Program.t
+  :  Minimal_syntax.Program.t
   -> Explicit_syntax.Program.t Or_error.t
