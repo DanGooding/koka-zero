e533a9ead7f6f2cd741fb725cef738f3b66d65a3
Dan Gooding <dan.gooding2000@gmail.com>
2025-07-10 20:53:03 +0100

    Allow per-node/edge attributes

--- lib/phases/02-subtyping-inference/dot_graph.mli
+++ lib/phases/02-subtyping-inference/dot_graph.mli
@@ -13,14 +13,19 @@ module Edge_disambiguator : sig
   val of_string : string -> t
 end
 
+module Attrs : sig
+  type t = (string * string) list [@@deriving sexp_of]
+end
+
 type t [@@deriving sexp_of]
 
 val create : unit -> t
-val add_node : t -> Node_id.t -> unit
+val add_node : ?attrs:Attrs.t -> t -> Node_id.t -> unit
 
 (* will add the nodes if they don't exist *)
 val add_edge
   :  ?disambiguator:Edge_disambiguator.t
+  -> ?attrs:Attrs.t
   -> t
   -> from:Node_id.t
   -> to_:Node_id.t
