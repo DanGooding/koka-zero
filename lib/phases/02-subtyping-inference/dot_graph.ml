871e0ee280fcacb993816642fedf219d8572bb1d
Dan Gooding <dan.gooding2000@gmail.com>
2025-07-10 20:53:03 +0100

    Allow per-node/edge attributes

--- lib/phases/02-subtyping-inference/dot_graph.ml
+++ lib/phases/02-subtyping-inference/dot_graph.ml
@@ -1,6 +1,17 @@
 open! Core
 open! Import
 
+module Attrs = struct
+  type t = (string * string) list [@@deriving sexp_of]
+
+  let empty = []
+
+  let to_attrs (t : t) : Odot.attr list =
+    List.map t ~f:(fun (key, value) ->
+      Odot.Double_quoted_id key, Some (Odot.Double_quoted_id value))
+  ;;
+end
+
 module Node_id = struct
   include String
 
@@ -9,9 +20,9 @@ module Node_id = struct
     Double_quoted_id t, port
   ;;
 
-  let to_stmt t : Odot.stmt =
+  let node_stmt t ~(attrs : Attrs.t) : Odot.stmt =
     let id = to_node_id t in
-    let attrs = [] in
+    let attrs = Attrs.to_attrs attrs in
     Stmt_node (id, attrs)
   ;;
 end
@@ -33,36 +44,44 @@ module Edge_id = struct
 
   let create ?disambiguator () ~from ~to_ = { from; to_; disambiguator }
 
-  let to_stmt t : Odot.stmt =
-    let from_id = Node_id.to_node_id t.from in
-    let to_id = Node_id.to_node_id t.to_ in
-    let attrs = [] in
+  let edge_stmt (id : t) ~attrs : Odot.stmt =
+    let from_id = Node_id.to_node_id id.from in
+    let to_id = Node_id.to_node_id id.to_ in
+    let attrs = Attrs.to_attrs attrs in
     Stmt_edge (Edge_node_id from_id, [ Edge_node_id to_id ], attrs)
   ;;
 end
 
 type t =
-  { nodes : Node_id.Hash_set.t
-  ; edges : Edge_id.Hash_set.t
+  { nodes : Attrs.t Node_id.Table.t
+  ; edges : Attrs.t Edge_id.Table.t
   }
 [@@deriving sexp_of]
 
 let create () =
-  let nodes = Node_id.Hash_set.create () in
-  let edges = Edge_id.Hash_set.create () in
+  let nodes = Node_id.Table.create () in
+  let edges = Edge_id.Table.create () in
   { nodes; edges }
 ;;
 
-let add_node t node_id = Hash_set.add t.nodes node_id
+let add_node ?(attrs = Attrs.empty) t node_id =
+  Hashtbl.set t.nodes ~key:node_id ~data:attrs
+;;
 
-let add_edge ?disambiguator t ~from ~to_ =
+let add_edge ?disambiguator ?(attrs = Attrs.empty) t ~from ~to_ =
   let edge_id = Edge_id.create ?disambiguator () ~from ~to_ in
-  Hash_set.add t.edges edge_id
+  Hashtbl.set t.edges ~key:edge_id ~data:attrs
 ;;
 
 let to_graph t : Odot.graph =
-  let nodes = Hash_set.to_list t.nodes |> List.map ~f:Node_id.to_stmt in
-  let edges = Hash_set.to_list t.edges |> List.map ~f:Edge_id.to_stmt in
+  let nodes =
+    Hashtbl.to_alist t.nodes
+    |> List.map ~f:(fun (node_id, attrs) -> Node_id.node_stmt node_id ~attrs)
+  in
+  let edges =
+    Hashtbl.to_alist t.edges
+    |> List.map ~f:(fun (edge_id, attrs) -> Edge_id.edge_stmt edge_id ~attrs)
+  in
   { strict = false; kind = Digraph; id = None; stmt_list = nodes @ edges }
 ;;
 
