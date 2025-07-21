570af687ea4d8d0cdbfb79251e7adc2c799a0bff
Dan Gooding <dan.gooding2000@gmail.com>
2025-07-21 22:36:11 +0100

    Capture and check output from clang

--- lib/phases/05-clang/compile.ml
+++ lib/phases/05-clang/compile.ml
@@ -1,10 +1,35 @@
 open! Core
 open! Import
 
-let run () ~prog ~args =
-  let child_pid = Core_unix.fork_exec () ~prog ~argv:(prog :: args) in
-  let result = Core_unix.waitpid child_pid in
-  Core_unix.Exit_or_signal.or_error result
+let run () ~prog ~args ~allow_output =
+  let { Core_unix.Process_info.pid; stdin = _; stdout; stderr } =
+    Core_unix.create_process ~prog ~args
+  in
+  let output = Core_unix.in_channel_of_descr stdout in
+  let error_output = Core_unix.in_channel_of_descr stderr in
+  let result = Core_unix.waitpid pid in
+  let output = In_channel.input_all output in
+  let error_output = In_channel.input_all error_output in
+  let result = Core_unix.Exit_or_signal.or_error result in
+  match result, output, error_output with
+  | Ok (), "", "" -> Ok ()
+  | Ok (), _stdout, "" when allow_output -> Ok ()
+  | Ok (), stdout, "" ->
+    Or_error.error_s
+      [%message
+        "unexpected output when running"
+          (prog : string)
+          (args : string list)
+          (stdout : string)]
+  | ((Ok () | Error _) as result), stdout, stderr ->
+    Or_error.error_s
+      [%message
+        "error when running"
+          (prog : string)
+          (args : string list)
+          (result : unit Or_error.t)
+          (stdout : string)
+          (stderr : string)]
 ;;
 
 let compile_ir_to_exe
@@ -30,5 +55,5 @@ let compile_ir_to_exe
   let flags = warning_flags @ opt_flag @ gc_flags in
   let inputs = [ ir_filename; config.runtime_path ] in
   let args = flags @ inputs @ [ "-o"; exe_filename ] in
-  run () ~prog:config.clang_exe ~args
+  run () ~prog:config.clang_exe ~args ~allow_output:false
 ;;
