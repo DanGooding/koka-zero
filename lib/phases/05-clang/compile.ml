dd58cf47a815f11e56521cadb49687d2519d6470
Dan Gooding <dan.gooding2000@gmail.com>
2025-08-11 21:09:05 +0100

    Fix library order linking

--- lib/phases/05-clang/compile.ml
+++ lib/phases/05-clang/compile.ml
@@ -39,21 +39,22 @@ let compile_ir_to_exe
       ~optimise
   =
   let opt_flag = Option.some_if optimise "-O3" |> Option.to_list in
-  let gc_flags =
+  let gc_flags, gc_lib =
     match config.gc_path with
     | None ->
       eprintf "compiling without garbage collector\n";
-      []
+      [], []
     | Some gc_path ->
-      [ "-DENABLE_GC"
-      ; [%string "-L%{gc_path}/lib"]
-      ; [%string "-I%{gc_path}/include"]
-      ; "-lgc"
-      ]
+      ( [ "-DENABLE_GC"
+        ; [%string "-L%{gc_path}/lib"]
+        ; [%string "-I%{gc_path}/include"]
+        ]
+      , [ "-lgc" ] )
   in
   let warning_flags = [ "-Wall"; "-Wno-override-module" ] in
   let flags = warning_flags @ opt_flag @ gc_flags in
   let inputs = [ ir_filename; config.runtime_path ] in
-  let args = flags @ inputs @ [ "-o"; exe_filename ] in
+  (* it's important for linking that gc library is listed _after_ user code *)
+  let args = flags @ inputs @ gc_lib @ [ "-o"; exe_filename ] in
   run () ~prog:config.clang_exe ~args ~allow_output:false
 ;;
